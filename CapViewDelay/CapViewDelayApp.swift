b26f12bd8c2bbbb231103618aea7aed1bc8d2734
Marc Tuinier <marctuinier@hotmail.com>
2023-06-21 20:18:05 +0200

    Initial Commit

---
+++ CapViewDelay/CapViewDelayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapViewDelayApp.swift
+//  CapViewDelay
+//
+//  Created by Marc Tuinier on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct CapViewDelayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
